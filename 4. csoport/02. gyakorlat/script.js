6f69ec0d3959a27976f19d63a5ba62d0234ddd15
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2022-02-11 14:25:43 +0100

    asd

---
+++ 4. csoport/02. gyakorlat/script.js
@@ -0,0 +1,21 @@
+const videogames = [
+    {
+        title: "League of Legends",
+        year: 2009,
+        genre: "MOBA",
+        publisher: "Riot Games",
+    }, 
+    {
+        title: "Rocket League",
+        year: 2015,
+        genre: "Sports",
+        publisher: "Psyonixapex",
+    },
+    {
+        title: "Apex Legends",
+        year: 2019,
+        genre: "Battle Royal",
+        publisher: "EA",
+    },
+
+]
